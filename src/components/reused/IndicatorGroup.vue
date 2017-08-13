e95520143ec280197c8c8a7ef8617f02858c1f05
Roger W <aznrogerazn@gmail.com>
2017-08-13 22:34:39 +0800

    <IndicatorGroup> added

---
+++ src/components/reused/IndicatorGroup.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="">
+    
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'IndicatorGroup',
+    data () {
+        return {
+
+        }
+    },
+    props: {
+        contents: {
+            required: false,
+            type: Object
+        }
+    },
+    methods: {
+        getTranslation (name) {
+            if (name.indexOf('.') != -1) {
+                // Process object tree
+                let ns = name.split('.');
+                let ref = LANG[ns[0]];
+                for (let i = 1; i < ns.length; ++i ) {
+                    // Tree into the object
+                    ref = ref[ns[i]];
+                }
+                return ref;
+            }
+            return LANG[name];
+        }
+    }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+@import '../shared'
+
+</style>
